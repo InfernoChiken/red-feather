4f7bd19cb496ecc060de4377d17aad486fac5ebb
InfernoChiken <81037231+InfernoChiken@users.noreply.github.com>
2021-04-25 11:49:31 -0500

    Crazy red feather

--- sketch.js
+++ sketch.js
@@ -3,33 +3,42 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1;
-var pig;
-var log;
-var bird;
+var box1, pig1;
+var bg;
+
+function preload(){
+    bg = loadImage("sprites/bg.png");
+}
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
-    box1 = new Box(700,350,70,70);
-    box2 = new Box(850,350,70,70);
-    ground = new Ground(600,height,1200,20);
-    pig = new Pig(775, 350);
-    log = new Log(775, 300, 250, PI/2);
-    box3 = new Box(700,250,70,70);
-    box4 = new Box(850,250,70,70);
-    pig2 = new Pig(775, 250);
-    log2 = new Log(775, 200, 250, PI/2);
-    box5 = new Box(780, 150,70,70);
-    log3 = new Log(745, 125, 120, PI/7);
-    log4 = new Log(800, 125, 125, PI/-7);
-    bird = new Birb(200, 1);
+    
+    ground = new Ground(600,height,1200,20)
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(100,100);
+
 }
 
 function draw(){
-    background(0);
+    background(bg);
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
@@ -37,14 +46,17 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
-    pig.display();
-    log.display();
+    pig1.display();
+    log1.display();
+
     box3.display();
     box4.display();
-    pig2.display();
-    log2.display();
-    box5.display();
+    pig3.display();
     log3.display();
+
+    box5.display();
     log4.display();
+    log5.display();
+
     bird.display();
 }
